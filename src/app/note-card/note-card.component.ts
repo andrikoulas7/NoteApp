906ec88185325a77cea895e6928432c6e8ffc1eb
andrikoulas7 <panagandrea@gmail.com>
2021-01-08 16:16:36 +0200

    First commit

---
+++ src/app/note-card/note-card.component.ts
@@ -0,0 +1,36 @@
+import { Component, ElementRef, Input, OnInit, Output, Renderer2, ViewChild, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-note-card',
+  templateUrl: './note-card.component.html',
+  styleUrls: ['./note-card.component.scss']
+})
+export class NoteCardComponent implements OnInit {
+
+  @Input() title: string;
+  @Input() body: string;
+  @Input() link: string;
+
+  @Output() deleteEvent = new EventEmitter<any>();
+
+  @ViewChild('truncator') truncator: ElementRef<HTMLElement>;
+  @ViewChild('bodyText') bodyText: ElementRef<HTMLElement>;
+
+  constructor(private renderer: Renderer2) { }
+
+  ngOnInit() {
+    let style = window.getComputedStyle(this.bodyText.nativeElement, null);
+    let viewableHeight = parseInt(style.getPropertyValue("height"), 10);
+
+    if (this.bodyText.nativeElement.scrollHeight > viewableHeight) {
+      this.renderer.setStyle(this.truncator.nativeElement, 'display', 'block');
+    } else {
+      this.renderer.setStyle(this.truncator.nativeElement.scrollHeight, 'display', 'none');
+    }
+  }
+
+  onXButtonClick() {
+    this.deleteEvent.emit();
+  }
+
+}
